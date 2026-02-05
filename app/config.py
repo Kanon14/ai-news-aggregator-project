c992019c49e01745bb76b7f3663b5718383f34b1
Kanon14 <geochua144@gmail.com>
2026-02-05 12:21:19 +0800

    added anthropic_scraper and basic runner testing

---
+++ app/config.py
@@ -0,0 +1,6 @@
+YOUTUBE_CHANNELS = [
+    "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
+    # "UCXl4i9dYBrFOabk0xGmbkRA", # Dwarkesh Patel
+    # "UCNJ1Ymd5yFuUPtn21xtRbbw", # AI Explained
+    # "UChpleBmo18P08aKCIgti38g" # Matt Wolfe
+]
