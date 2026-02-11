6b163dbb7b06c3de41c19f2a34eb9548dd93a266
Kanon14 <geochua144@gmail.com>
2026-02-11 15:09:09 +0800

    modified migrate file

--- api/migrate.py
+++ api/migrate.py
@@ -1,18 +1,20 @@
-﻿from http.server import BaseHTTPRequestHandler
-import json
-from app.database.models import Base
-from app.database.connection import engine
+﻿# UNCOMMENT AND TO RUN ONCE IN THE VERCEL PRODUCTION URL
+# DELETE IT AFTER TABLE CREATION SUCCESS AND REDEPLOY
+# from http.server import BaseHTTPRequestHandler
+# import json
+# from app.database.models import Base
+# from app.database.connection import engine
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        try:
-            Base.metadata.create_all(engine)
-            body = json.dumps({"ok": True, "message": "Migrations ran successfully."}).encode("utf-8")
-            self.send_response(200)
-        except Exception as e:
-            body = json.dumps({"ok": False, "error": str(e)}).encode("utf-8")
-            self.send_response(500)
-        self.send_header("Content-Type", "application/json")
-        self.send_header("Content-Length", str(len(body)))
-        self.end_headers()
-        self.wfile.write(body)
+# class handler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         try:
+#             Base.metadata.create_all(engine)
+#             body = json.dumps({"ok": True, "message": "Migrations ran successfully."}).encode("utf-8")
+#             self.send_response(200)
+#         except Exception as e:
+#             body = json.dumps({"ok": False, "error": str(e)}).encode("utf-8")
+#             self.send_response(500)
+#         self.send_header("Content-Type", "application/json")
+#         self.send_header("Content-Length", str(len(body)))
+#         self.end_headers()
+#         self.wfile.write(body)
